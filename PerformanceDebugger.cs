a5f7f5a8e7745135d71398a919ab95da229e393c
AtomicHeadd <58561188+AtomicHeadd@users.noreply.github.com>
2022-08-14 23:37:56 +0900

    cleaning

--- PerformanceDebugger.cs
+++ PerformanceDebugger.cs
@@ -14,6 +14,7 @@ public class PerformanceDebugger : MonoBehaviour
     [SerializeField] float averagePeriodSec = 60;
 
     Queue<float> frameQueue;
+    float acceptableFrames;
     float sum1minutes;
 
     void Start()
@@ -24,33 +25,38 @@ public class PerformanceDebugger : MonoBehaviour
         StartCoroutine(GetPerformanceText(debugIntervalSec));
     }
 
-    // Update is called once per frame
-    void Update()
+    string GetThisFrameRate(float acceptableFrames)
     {
-
+        int frames = Mathf.FloorToInt(1f / Time.deltaTime);
+        string color = frames > acceptableFrames ? "green" : "red";
+        return "fps: " + ColorizeText($"{frames}", color);
     }
 
-    public IEnumerator GetPerformanceText(float debugIntervalSec){
-        int frames = Mathf.FloorToInt(1f / Time.deltaTime);
-        string color = (float)frames > targetFrameRate * acceptableRate ? "green" : "red";
-        string current = $"<color={color}>{frames}</color>";
-        if(!showAverage){
-            frameRate.text = current;
-            yield return new WaitForSeconds(debugIntervalSec);
-            StartCoroutine(GetPerformanceText(debugIntervalSec));
-            yield break;
-        }
+    string GetAverageFrameRate(float acceptableFrames)
+    {
         sum1minutes += 1f / Time.deltaTime;
         frameQueue.Enqueue(1f/Time.deltaTime);
         if(frameQueue.Count > averagePeriodSec / debugIntervalSec) {
             float de = frameQueue.Dequeue();
             sum1minutes -= de;
-            print($"Dequeue開始 count: {frameQueue.Count}");
+            //print($"Dequeue開始 count: {frameQueue.Count}");
         }
         float average = sum1minutes / frameQueue.Count;
         string averageColor = average > targetFrameRate * acceptableRate ? "green" : "red";
-        string averageText = $"\n average: <color={averageColor}>{Mathf.FloorToInt(average)}</color>";
-        frameRate.text = current + averageText;
+        string averageText = $"average: <color={averageColor}>{Mathf.FloorToInt(average)}</color>";
+        return averageText;
+    }
+
+    string ColorizeText(string text, string color)
+    {
+        return $"<color={color}>{text}</color>";
+    }
+
+    public IEnumerator GetPerformanceText(float debugIntervalSec)
+    {
+        string frameRateText = GetThisFrameRate(targetFrameRate * acceptableRate);
+        if(showAverage) frameRateText += $"\n{GetAverageFrameRate(targetFrameRate * acceptableRate)}";
+        frameRate.text = frameRateText;
         yield return new WaitForSeconds(debugIntervalSec);
         StartCoroutine(GetPerformanceText(debugIntervalSec));
     }
