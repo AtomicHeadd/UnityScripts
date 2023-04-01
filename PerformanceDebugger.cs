83087d88e13a625a4ad49ca7e2f3f501b0a97a3f
AtomicHeadd <58561188+AtomicHeadd@users.noreply.github.com>
2023-04-02 08:04:56 +0900

    Update PerformanceDebugger.cs

--- PerformanceDebugger.cs
+++ PerformanceDebugger.cs
@@ -3,31 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+[RequireComponent(typeof(TextMeshProUGUI))]
 public class PerformanceDebugger : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI frameRate;
-    [SerializeField] float debugIntervalSec = 1;
+    TextMeshProUGUI frameRate;
+    [SerializeField] float textUpdateInterval = .1f;
     [SerializeField] int targetFrameRate = 60;
-    [SerializeField] float acceptableRate = .8f;
+    [SerializeField] float fineThreshold = .8f;
 
     [SerializeField] bool showAverage = false;
     [SerializeField] float averagePeriodSec = 60;
 
     Queue<float> frameQueue;
-    float acceptableFrames;
     float sum1minutes;
 
     void Start()
     {
+        frameRate = GetComponent<TextMeshProUGUI>();
         Application.targetFrameRate = targetFrameRate; 
         frameQueue = new Queue<float>();
         sum1minutes = 0;
-        StartCoroutine(GetPerformanceText(debugIntervalSec));
+        StartCoroutine(GetPerformanceText(textUpdateInterval));
     }
 
     string GetThisFrameRate(float acceptableFrames)
     {
-        int frames = Mathf.FloorToInt(1f / Time.deltaTime);
+        int frames = Mathf.RoundToInt(1f / Time.deltaTime);
         string color = frames > acceptableFrames ? "green" : "red";
         return "fps: " + ColorizeText($"{frames}", color);
     }
@@ -36,14 +37,13 @@ public class PerformanceDebugger : MonoBehaviour
     {
         sum1minutes += 1f / Time.deltaTime;
         frameQueue.Enqueue(1f/Time.deltaTime);
-        if(frameQueue.Count > averagePeriodSec / debugIntervalSec) {
+        if(frameQueue.Count > averagePeriodSec / textUpdateInterval) {
             float de = frameQueue.Dequeue();
             sum1minutes -= de;
-            //print($"Dequeue開始 count: {frameQueue.Count}");
         }
         float average = sum1minutes / frameQueue.Count;
-        string averageColor = average > targetFrameRate * acceptableRate ? "green" : "red";
-        string averageText = $"average: <color={averageColor}>{Mathf.FloorToInt(average)}</color>";
+        string averageColor = average > targetFrameRate * fineThreshold ? "green" : "red";
+        string averageText = $"average: <color={averageColor}>{Mathf.RoundToInt(average)}</color>";
         return averageText;
     }
 
@@ -54,8 +54,8 @@ public class PerformanceDebugger : MonoBehaviour
 
     public IEnumerator GetPerformanceText(float debugIntervalSec)
     {
-        string frameRateText = GetThisFrameRate(targetFrameRate * acceptableRate);
-        if(showAverage) frameRateText += $"\n{GetAverageFrameRate(targetFrameRate * acceptableRate)}";
+        string frameRateText = GetThisFrameRate(targetFrameRate * fineThreshold);
+        if(showAverage) frameRateText += $"\n{GetAverageFrameRate(targetFrameRate * fineThreshold)}";
         frameRate.text = frameRateText;
         yield return new WaitForSeconds(debugIntervalSec);
         StartCoroutine(GetPerformanceText(debugIntervalSec));
